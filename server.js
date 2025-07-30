ffe2d20ce053f871cb6c44bbd0b12d48f744f346
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-30 19:51:46 -0400

    idk

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const qs = require("querystring");
 const { createServer } = require("node:http");
 
+// All JSON data, in requests or responses, must be encoded using UTF-8.
+
 const server = createServer((req, res) => {
 
     console.log(req.method + " : " + req.url);
@@ -10,7 +12,7 @@ const server = createServer((req, res) => {
     res.end("404 Internal Server Error");
 });
 
-server.listen(3000, "127.0.0.1", () => {
+server.listen(8448, "127.0.0.1", () => { // port for SSL
 
-    console.log(`Starting @ http://127.0.0.1:3000/`);
+    console.log(`Starting @ http://127.0.0.1:8448/`);
 });
