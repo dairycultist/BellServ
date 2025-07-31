626c741fba02b315f40ad889ae57701967fcc289
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-30 22:50:09 -0400

    Switch to https

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const fs = require("fs");
-const qs = require("querystring");
-const { createServer } = require("node:http");
+// const qs = require("querystring");
+const { createServer } = require("node:https");
 
-// All JSON data, in requests or responses, must be encoded using UTF-8.
+const options = {
+    key: fs.readFileSync('private.key.pem'), // path to ssl PRIVATE key from Porkbun
+    cert: fs.readFileSync('domain.cert.pem'),// path to ssl certificate from Porkbun
+};
 
-const server = createServer((req, res) => {
+const server = createServer(options, (req, res) => {
+
+    // All JSON data, in requests or responses, must be encoded using UTF-8.
 
     console.log(req.method + " : " + req.url);
 
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Internal Server Error");
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end("{}");
 });
 
-server.listen(8448, "127.0.0.1", () => { // port for SSL
+server.listen(443, () => {
 
-    console.log(`Starting @ http://127.0.0.1:8448/`);
+    console.log(`Starting @ http://0.0.0.0:443/`);
 });
+
