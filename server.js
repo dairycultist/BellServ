7341165fd60ab0041c756c4853afb83187d80e1d
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-01 00:18:30 -0400

    Begin handling POST requests

--- server.js
+++ server.js
@@ -6,39 +6,7 @@ const { createServer } = require("node:http"); // https
 //     cert: fs.readFileSync("../domain.cert.pem"),// path to ssl certificate from Porkbun
 // };
 
-const server = createServer((req, res) => { // options before () for https
-
-    var status = 404;
-    var body = {};
-
-    switch (req.method + " " + req.url) {
-
-        case "GET /_matrix/client/versions":
-            status = 200;
-            body = {
-                "versions": [
-                    "v1.15"
-                ]
-            };
-            break;
-
-        case "GET /_matrix/client/v3/login":
-            status = 200;
-            body = {
-                "flows": [
-                    { "type": "m.login.password" }
-                ]
-            };
-            break;
-
-        case "OPTIONS /_matrix/client/v3/login":
-            status = 204; // No Content, just needs info from headers
-            break;
-        
-        case "POST /_matrix/client/v3/login":
-            console.log(req);
-            break;
-    }
+function respond(req, res, status, body) {
 
     res.writeHead(status, {
         "Access-Control-Allow-Origin": "*",
@@ -50,6 +18,63 @@ const server = createServer((req, res) => { // options before () for https
     res.end(JSON.stringify(body));
 
     console.log("(" + status + ") " + req.method + " " + req.url);
+}
+
+function passReqBody(req, func) {
+
+    let body = "";
+
+    req.on("data", chunk => {
+        body += chunk.toString();
+    });
+
+    req.on("end", () => {
+        try {
+            func(JSON.parse(body));
+        } catch (error) {
+            respond(req, res, 400, {}); // invalid request (bad JSON)
+        }
+    });
+}
+
+const server = createServer((req, res) => { // options before () for https
+
+    switch (req.method + " " + req.url) {
+
+        case "GET /_matrix/client/versions":
+            respond(req, res, 200, {
+                "versions": [
+                    "v1.15"
+                ]
+            });
+            return;
+
+        case "GET /_matrix/client/v3/login":
+            respond(req, res, 200, {
+                "flows": [
+                    { "type": "m.login.password" }
+                ]
+            });
+            return;
+
+        case "OPTIONS /_matrix/client/v3/login":
+            respond(req, res, 204, {}); // 204 = No Content, just needs info from headers
+            return;
+        
+        case "POST /_matrix/client/v3/login":
+            passReqBody(req, (json) => {
+                
+                if (json.type == "m.login.password") {
+                    console.log(json);
+                } else {
+                    respond(req, res, 400, {}); // invalid request (wrong login type)
+                }
+            });
+            return;
+    }
+
+    // default response
+    respond(req, res, 404, {});
 });
 
 server.listen(3000, "localhost", () => { // 443 for https
