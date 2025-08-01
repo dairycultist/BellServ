47476458458ec0bcd78e9d33c0a0586ec035af2a
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-31 23:51:46 -0400

    Start handling some headers

--- server.js
+++ server.js
@@ -21,13 +21,31 @@ const server = createServer((req, res) => { // options before () for https
                 ]
             };
             break;
+
+        case "GET /_matrix/client/v3/login":
+            status = 200;
+            body = {
+                "flows": [
+                    { "type": "m.login.password" }
+                ]
+            };
+            break;
+
+        case "OPTIONS /_matrix/client/v3/login":
+            status = 204; // No Content, just needs info from headers
+            break;
+        
+        case "POST /_matrix/client/v3/login":
+            console.log(req);
+            break;
     }
 
     res.writeHead(status, {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-        'Access-Control-Max-Age': 2592000, // 30 days
-        'Content-Type': 'application/json; charset=utf-8'
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Max-Age": 2592000, // 30 days
+        "Content-Type": "application/json; charset=utf-8"
     });
     res.end(JSON.stringify(body));
 
