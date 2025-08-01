5b1fe12e9e9916e213736de1250ef9982f7032ba
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-31 23:35:23 -0400

    Go back to testing locally because the server doesn't have port forwarding enabled by default and I want to cry

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const fs = require("fs");
-// const qs = require("querystring");
-const { createServer } = require("node:https");
+const { createServer } = require("node:http"); // https
 
-const options = {
-    key: fs.readFileSync("../private.key.pem"), // path to ssl PRIVATE key from Porkbun
-    cert: fs.readFileSync("../domain.cert.pem"),// path to ssl certificate from Porkbun
-};
+// const options = {
+//     key: fs.readFileSync("../private.key.pem"), // path to ssl PRIVATE key from Porkbun
+//     cert: fs.readFileSync("../domain.cert.pem"),// path to ssl certificate from Porkbun
+// };
 
-const server = createServer(options, (req, res) => {
+const server = createServer((req, res) => { // options before () for https
 
     var status = 404;
     var body = {};
@@ -18,37 +17,25 @@ const server = createServer(options, (req, res) => {
             status = 200;
             body = {
                 "versions": [
-                    "r0.0.1",
-                    "r0.1.0",
-                    "r0.2.0",
-                    "r0.3.0",
-                    "r0.4.0",
-                    "r0.5.0",
-                    "r0.6.0",
-                    "r0.6.1",
-                    "v1.1",
-                    "v1.2",
-                    "v1.3",
-                    "v1.4",
-                    "v1.5",
-                    "v1.8",
-                    "v1.11",
-                    "v1.12",
-                    "v1.13",
-                    "v1.14"
+                    "v1.15"
                 ]
             };
             break;
     }
 
-    res.writeHead(status, { "Content-Type": "application/json; charset=utf-8" });
+    res.writeHead(status, {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+        'Access-Control-Max-Age': 2592000, // 30 days
+        'Content-Type': 'application/json; charset=utf-8'
+    });
     res.end(JSON.stringify(body));
 
     console.log("(" + status + ") " + req.method + " " + req.url);
 });
 
-server.listen(443, () => {
+server.listen(3000, "localhost", () => { // 443 for https
 
-    console.log(`Starting @ http://0.0.0.0:443/`);
+    console.log(`Starting @ http://localhost:3000/`);
 });
 
